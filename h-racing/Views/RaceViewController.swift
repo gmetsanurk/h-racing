5a46b11e35f7a9ef7e7338a610830530d48b5816
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-04-16 13:52:59 +0300

    Do startButton and resetButton

--- h-racing/Views/RaceViewController.swift
+++ h-racing/Views/RaceViewController.swift
@@ -5,3 +5,22 @@
 //  Created by Georgy on 2025-04-16.
 //
 
+import UIKit
+
+class RaceViewController: UIViewController {
+    let startButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Start", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    let resetButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Restart", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true
+        return button
+    }()
+}
